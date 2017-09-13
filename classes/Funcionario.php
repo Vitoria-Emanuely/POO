74ad2174cac81bcffb6b4fcc33305a45bd49b395
Vitoria-Emanuely <vikaemanuely@gmail.com>
2017-09-13 11:05:45 -0300

    Add files via upload

--- classes/Funcionario.php
+++ classes/Funcionario.php
@@ -11,23 +11,34 @@ class Funcionario
     public $nome;
     public $departmento;
     public $salario = 0;
-    public $dataEntrada;
+    private $dataEntrada;
     public $CPF;
     const SALARIO_ANUAL_E_DECIMO = 13;
     const FERIAS = 0.3;
-
     public function recebeAumento(float $aumento){
-
         $this->salario += $aumento;
-
     }
-
     public function calculaGanhoAnual():float {
-        return ($this->salario * SALARIO_ANUAL_E_DECIMO) + $this->salario * FERIAS;
+        return ($this->salario * self::SALARIO_ANUAL_E_DECIMO) + $this->salario * self::FERIAS;
+    }
+    public function mostra(){
+        echo "Nome: $this->nome \n";
+        echo "Departamento: $this->departmento \n";
+        echo "Salario: $this->salario \n";
+        echo "Ganho anual: " .$this->calculaGanhoAnual(). "\n";
+        echo "Data de entrada: " .$this->dataEntrada->formadata();
+
     }
 
-    public function mostra(){
-        //TODO fazer o método mostra como pede o exercício
+    public function getDataEntrada()
+    {
+        return $this->dataEntrada;
     }
+
+    public function setDataEntrada(Data $dataEntrada)
+    {
+        $this->dataEntrada = $dataEntrada;
+    }
+
 
 }
